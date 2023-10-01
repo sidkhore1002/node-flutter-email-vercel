4df67edfc11a88137c3e7c77d93aa1ca6055e4e8
siddharth khore <35920894+sidkhore1002@users.noreply.github.com>
2023-10-01 19:25:50 +0530

    node-flutter-email-vercel
    node-flutter-email-vercel

---
+++ index.js
@@ -0,0 +1,48 @@
+const nodemailer = require('nodemailer');
+const http = require('http')
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+
+app.listen(5000, console.log("Server started on port: 5000") )
+
+app.use(bodyParser.json());
+
+app.get('/', function(req,res){
+    console.log("Flutter-Nodejs-Email...")
+    res.send("Flutter - Nodejs - Email...")
+    res.end()
+})
+
+app.post('/sendemail', function(req, res){
+    console.log(req.body.emailId)    
+    sendEmail(req.body.emailId)
+    res.send("Email sent...")
+    res.end()
+});
+
+function sendEmail(emailId){
+    var transpoter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'findoutmobile@gmail.com',
+            pass: 'eiorarwtsqtyxaee'        
+        }
+    });
+    var mailOptions = {
+        from : 'findoutmobile@gmail.com',
+        to: emailId,
+        subject: 'Flutter-Nodejs test email',
+        text: 'Flutter-Nodejs test email'
+    }
+    transpoter.sendMail(mailOptions, function(error, info){
+        if(error){
+            console.log(error);
+        }
+        else{
+            console.log("Email sent.. " + info.envelope);
+        }
+    });
+}
+
+
